ac328b372ef1f00a7b504a32ef953121c5d286e8
Jason Hicks <hicks114@live.missouristate.edu>
2013-02-09 18:47:54 -0600

    4.2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,11 +12,14 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', 
-        :text => "#{base_title} | Home")
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
 
   end
